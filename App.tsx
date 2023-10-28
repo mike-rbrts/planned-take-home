27bfb0f7e5ef580e8a9e8a6f82165c3385b0c6e2
Michael Roberts <roberts.b.michael@gmail.com>
2023-10-28 16:44:51 -0500

    MR | Refactored component, added type safety, corrected styles

--- App.tsx
+++ App.tsx
@@ -1,58 +1,96 @@
-import { Image, InteractionManager, SafeAreaView, StyleSheet, View } from 'react-native';
+import {Image, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
-const ProfileImage = ({ url }) => {
-  <View style={styles.profileCircle}>
-    <Image source={} uri={{ source: cat.imageUrl }} />
-  </View>
+type ProfileImageProps = {
+  imageUrl: string;
 };
 
-const EmptyView = () => (
-  <View style={styles.emptyContainer}>
-    No cats😿
-  </View>
-);
+type RenderProfileProps = {
+  cats: Cat[];
+};
 
-export default function App() {
-  const listOfCats = [
-    { id: 0, name: 'Marge', imageUrl: 'http://placekitten.com/200/300' },
-    { id: 1, name: 'Furball', imageUrl: 'http://placekitten.com/300/400' },
-    { id: 2, name: 'Football', imageUrl: 'http://placekitten.com/300/200' },
-    { id: 3, name: 'Dot', imageUrl: 'http://placekitten.com/400/300' },
-    { id: 3, name: 'Zippy', imageUrl: 'http://placekitten.com/400/400' },
-  ];
+interface Cat {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
 
+const ProfileImage: React.FC<ProfileImageProps> = (props) => {
   return (
-    <SafeAreaView style={styles.container}>
-      {listOfCats.length && listOfCats.map(cat => ( <View style={styles.catContainer}>
-        <ProfileImage url={cat.imageUrl}>
+    <View style={styles.profileCircle}>
+      <Image style={styles.image} source={{ uri: props.imageUrl }} />
+    </View>
+  );
+};
+
+const EmptyView: React.FC = () => {
+  return (
+    <View style={styles.emptyContainer}>
+      <Text>No cats😿</Text>
+    </View>
+  );
+};
+
+const RenderProfile: React.FC<RenderProfileProps> = (props) => {
+  if (props.cats.length) {
+    return props.cats.map((cat) => (
+      <View key={cat.id} style={styles.profileContainer}>
+        <ProfileImage imageUrl={cat.imageUrl} />
         <Text style={styles.nameText}>{cat.name}</Text>
       </View>
-      ))}
-      {!listOfCats.length && <EmptyView />} </SafeAreaView>
+    ));
+  } else {
+    return <EmptyView />;
+  }
+};
+
+const listOfCats: Cat[] = [
+  { id: 0, name: 'Marge', imageUrl: 'http://placekitten.com/200/300' },
+  { id: 1, name: 'Furball', imageUrl: 'http://placekitten.com/300/400' },
+  { id: 2, name: 'Football', imageUrl: 'http://placekitten.com/300/200' },
+  { id: 3, name: 'Dot', imageUrl: 'http://placekitten.com/400/300' },
+  { id: 4, name: 'Zippy', imageUrl: 'http://placekitten.com/400/400' },
+];
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <RenderProfile cats={listOfCats} />
+    </SafeAreaView>
   );
-}
-  
+};
+
 const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    marginTop: 25,
+  },
   profileContainer: {
     flexDirection: 'row',
-    alignItem: 'center',
-    padding: 8,
+    alignItems: 'center',
+    padding: Platform.OS === 'android' ? 25 : 8,
     borderBottomColor: 'gray',
   },
   profileCircle: {
     width: 100,
     height: 100,
-    borderRadius: '50',
+    borderRadius: 50,
     borderWidth: 2,
-    borderColor: 'marroon',
+    borderColor: 'maroon',
     marginRight: 20,
   },
   nameText: {
-    size: 24,
+    fontSize: 24,
   },
   emptyContainer: {
     flex: 1,
     justifyContent: 'center',
   },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 50,
+  },
 });
- 
+
+export default App;
